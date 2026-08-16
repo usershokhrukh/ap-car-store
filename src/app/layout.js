633e4dd5ad7b6c461fbc17ea4415358d0e4fb7f7
usershokhrukh <asxurovsit@gmail.com>
2026-08-17 00:05:11 +0500

    features/main notification changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
+      <body className="body">
         <GeneralModalProvide>
           <NotificationCustom>
             <AppLayout>{children}</AppLayout>
